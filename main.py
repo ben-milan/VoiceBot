b54cfd1d2861e16c5cf697781445f0867b1828f0
klenkb-bzz <klenkb@bzz.ch>
2024-04-12 15:23:47 +0200

    Errorhandling in AudioProccessing.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from AudioProccessing import *
 from BotLogic import *
 def main():
 
-   print("Hello Main!")
+    openWebsite()
+    speak_text("Herzlich Willkommen! Ich bin Ihr Sprachassistent auf ticketcorner punkt ch")
+    searchEvent()
+    orderProcesse()
+
 
 if __name__ == '__main__':
     main()
