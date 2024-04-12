abbc7b9703bb3a8b3f8106dfefdfdf85baddeb62
kollert-bzz <kollert@bzz.ch>
2024-04-12 14:22:36 +0200

    sort2

--- sort_data.py
+++ sort_data.py
@@ -1,23 +1,73 @@
+from test_data import generate_data
 import pandas as pd
 
+generate_data()
 sectors = ['A', 'B', 'C', 'D']
+rows = range(1, 11)
+prices = [20, 30, 40, 50]
+statuses = ['o', 'c']
 test_data_path = 'test_data.csv'
 
 
 def sort_by_sector():
-    sector_input = input('Gib einen Sektor an [A-D]\n>>> ')
-    sector_input = sector_input.upper()
-    while sector_input not in sectors:
-        print('ERROR: Dieser Sektor existiert nicht!')
-        sector_input = input('Gib einen Sektor an [A-D]\n>>> ')
-        sector_input = sector_input.upper()
+    sector_input = input('Gib einen oder mehrere Sektoren an [A-D, durch Komma getrennt, ohne Abstand]\n>>> ')
+    sector_input = sector_input.upper().split(',')
+    while not all(sector in sectors for sector in sector_input):
+        print('ERROR: Einer oder mehrere Sektoren existieren nicht!')
+        sector_input = input('Gib einen oder mehrere Sektoren an [A-D, durch Komma getrennt, ohne Abstand]\n>>> ')
+        sector_input = sector_input.upper().split(',')
 
     df = pd.read_csv(test_data_path)
-    filtered_df = df[df['Sektor'] == sector_input]
+    filtered_df = df[df['Sektor'].isin(sector_input)]
     filtered_df.to_csv(test_data_path, index=False)
 
+
+def sort_by_row():
+    row_input = input('Gib eine oder mehrere Reihen an [1-10, durch Komma getrennt, ohne Abstand]\n>>> ')
+    row_input = list(map(int, row_input.split(',')))
+    while not all(row in rows for row in row_input):
+        print('ERROR: Eine oder mehrere Reihen existieren nicht!')
+        row_input = input('Gib eine oder mehrere Reihen an [1-10, durch Komma getrennt, ohne Abstand]\n>>> ')
+        row_input = list(map(int, row_input.split(',')))
+
+    df = pd.read_csv(test_data_path)
+    filtered_df = df[df['Reihe'].isin(row_input)]
+    filtered_df.to_csv(test_data_path, index=False)
+
+
+def sort_by_price():
+    price_input = input('Gib einen oder mehrere Preise an [20, 30, 40, 50, durch Komma getrennt, ohne Abstand]\n>>> ')
+    price_input = list(map(int, price_input.split(',')))
+    while not all(price in prices for price in price_input):
+        print('ERROR: Eine oder mehrere Preise existieren nicht!')
+        price_input = input(
+            'Gib einen oder mehrere Preise an [20, 30, 40, 50, durch Komma getrennt, ohne Abstand]\n>>> ')
+        price_input = list(map(int, price_input.split(',')))
+
+    df = pd.read_csv(test_data_path)
+    filtered_df = df[df['Preis'].isin(price_input)]
+    filtered_df.to_csv(test_data_path, index=False)
+
+
+def sort_by_status():
+    status_input = input('Gib einen oder mehrere Status an [o, c, durch Komma getrennt, ohne Abstand]\n>>> ')
+    status_input = status_input.split(',')
+    while not all(status in statuses for status in status_input):
+        print('ERROR: Eine oder mehrere Status existieren nicht!')
+        status_input = input('Gib einen oder mehrere Status an [o, c, durch Komma getrennt, ohne Abstand]\n>>> ')
+        status_input = status_input.split(',')
+
+    df = pd.read_csv(test_data_path)
+    filtered_df = df[df['Status'].isin(status_input)]
+    filtered_df.to_csv(test_data_path, index=False)
+
+
+def all_sorts():
+    sort_by_sector()
+    sort_by_row()
+    sort_by_price()
+    sort_by_status()
     print('done')
 
 
-sort_by_sector()
-
+all_sorts()
--- test_data.py
+++ test_data.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import random
 
+
 def generate_data():
     return random.choice(["o", "c"])
+
 
 columns = ["Sektor", "Reihe", "Platz", "Preis", "Status"]
 sitzplan_df = pd.DataFrame(columns=columns)
@@ -10,15 +12,13 @@ sitzplan_df = pd.DataFrame(columns=columns)
 for sektor in ["A", "B", "C", "D"]:
     for reihe in range(1, 11):
         for platz in range(1, 21):
-            preis = 50 if sektor == "A" else 40 if sektor == "B" else 30 if sektor == "C" else 20
+            preis = random.choice([20, 30, 40, 50])
             status = generate_data()
             sitzplan_df = sitzplan_df._append({
-                                              "Sektor": sektor,
-                                              "Reihe": reihe,
-                                              "Platz": platz,
-                                              "Preis": preis,
-                                              "Status": status}, ignore_index=True)
+                "Sektor": sektor,
+                "Reihe": reihe,
+                "Platz": platz,
+                "Preis": preis,
+                "Status": status}, ignore_index=True)
 
 sitzplan_df.to_csv("test_data.csv", index=False)
-
-
