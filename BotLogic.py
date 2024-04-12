94da35bd3e67c603bb4b8ccbc50ebe9b2ac950e4
elias <defreitase@bzz.ch>
2024-04-12 12:56:10 +0200

    help ben

--- BotLogic.py
+++ BotLogic.py
@@ -1,30 +1,9 @@
 import pyautogui
 import time
-import win32gui
-
-
-
-def get_browser_handle():
-    # Adjust the title to match your browser window title
-    browser_title = "Google Chrome"
-    handle = win32gui.FindWindow(None, browser_title)
-    return handle
-
-
-def bring_to_front(handle):
-    win32gui.ShowWindow(handle, 5)  # SW_SHOW
-    win32gui.SetForegroundWindow(handle)
 
 
 def main():
-    time.sleep(2)
-
-    # Get the handle of the browser window
-    browser_handle = get_browser_handle()
-
-    # Bring the browser window to the front
-    bring_to_front(browser_handle)
-    pyautogui.hotkey('ctrl', 't')
+    openWebsite()
 
 
 def openWebsite():
@@ -32,6 +11,14 @@ def openWebsite():
 
     # Simulate pressing Ctrl + T to open a new tab
     pyautogui.hotkey('ctrl', 't')
+    pyautogui.write("https://www.ticketcorner.ch")
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+
+    x, y = pyautogui.locateCenterOnScreen("img/Search.png")
+    pyautogui.moveTo(x, y, 1)
+    pyautogui.click()
+    pyautogui.write("Knie")
 
 
 if __name__ == '__main__':
