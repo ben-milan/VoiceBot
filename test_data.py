f68c126d22200bb509e0a542e6168dcd58814ccb
kollert-bzz <kollert@bzz.ch>
2024-04-12 13:00:08 +0200

    test data added, with status

--- test_data.py
+++ test_data.py
@@ -1,26 +1,36 @@
+import random
+
 sitzplan = {}
 
 
-for i in range(1, 11):
-    for j in range(1, 21):
-        sitzplan[f"A{i}-{j}"] = {"Preis": 50}
+def generate_status():
+    return random.choice(["o", "c"])
 
 
 for i in range(1, 11):
     for j in range(1, 21):
-        sitzplan[f"B{i}-{j}"] = {"Preis": 40}
+        status = generate_status()
+        sitzplan[f"A{i}-{j}"] = {"Preis": 50, "Status": status}
 
 
 for i in range(1, 11):
     for j in range(1, 21):
-        sitzplan[f"C{i}-{j}"] = {"Preis": 30}
+        status = generate_status()
+        sitzplan[f"B{i}-{j}"] = {"Preis": 40, "Status": status}
 
 
 for i in range(1, 11):
     for j in range(1, 21):
-        sitzplan[f"D{i}-{j}"] = {"Preis": 20}
+        status = generate_status()
+        sitzplan[f"C{i}-{j}"] = {"Preis": 30, "Status": status}
 
-print(sitzplan)
+
+for i in range(1, 11):
+    for j in range(1, 21):
+        status = generate_status()
+        sitzplan[f"D{i}-{j}"] = {"Preis": 20, "Status": status}
+
+
 with open("test_data.txt", "w") as file:
     for sitzplatz, daten in sitzplan.items():
-        file.write(f"{sitzplatz}, {daten['Preis']}\n")
+        file.write(f"{sitzplatz}, {daten['Preis']}, {daten['Status']}\n")
